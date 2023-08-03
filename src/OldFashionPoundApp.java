db66dc32833974ead61c0984f7eff81631cb13a1
Cristina Acsinte <cristtina.acsinte@gmail.com>
2023-08-03 11:09:08 +0200

    prova commit

--- src/OldFashionPoundApp.java
+++ src/OldFashionPoundApp.java
@@ -1,6 +1,16 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class OldFashionPoundApp {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Pattern pattern = Pattern.compile("^\\s*(\\d+[p])(\\d+[s])\\s*(\\d+[p])\\s*([+\\-*/])\\s*(\\d+[p])(\\d+[s])\\s*(\\d+[p])\\*$", Pattern.CASE_INSENSITIVE);
+    Matcher matcher = pattern.matcher("5p 17s 12p + 3p 4s 10p");
+    boolean matchFound = matcher.find();
+    if(matchFound) {
+      System.out.println("Match found");
+    } else {
+      System.out.println("Match not found");
+    }
   }
 }
